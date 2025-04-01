d5fd515af3366fadd9d61b600d6b8186c19518b2
Don <serfdon@gmail.com>
2025-03-31 17:22:25 -0700

    Fixed texture

--- short_table.lua
+++ short_table.lua
@@ -3,10 +3,10 @@ minetest.register_node("myworkshop:short_table", {
 	tiles = {
 		"myworkshop_bench_top.png",
 		"myworkshop_wood.png",
-		"myworkshop_wood.png",
-		"myworkshop_wood.png",
-		"myworkshop_wood.png",
-		"myworkshop_wood.png",
+		"myworkshop_small_table_side.png",
+		"myworkshop_small_table_side.png",
+		"myworkshop_small_table_side.png",
+		"myworkshop_small_table_side.png",
 	},
 	drawtype = "nodebox",
 	paramtype = "light",
@@ -15,21 +15,21 @@ minetest.register_node("myworkshop:short_table", {
 	node_box = {
 		type = "fixed",
 		fixed = {
-			{-0.5, 0, -0.5, 0.5, 0.125, 0.5}, -- NodeBox10
-			{0.375, -0.5, 0.375, 0.5, 0.125, 0.5}, -- NodeBox11
-			{-0.5, -0.5, 0.375, -0.375, 0.125, 0.5}, -- NodeBox12
-			{-0.5, -0.5, -0.5, -0.375, 0.125, -0.375}, -- NodeBox13
-			{0.375, -0.5, -0.5, 0.5, 0.125, -0.375}, -- NodeBox14
+			{-0.5, 0, -0.5, 0.5, 0.125, 0.5},
+			{0.375, -0.5, 0.375, 0.5, 0.125, 0.5},
+			{-0.5, -0.5, 0.375, -0.375, 0.125, 0.5},
+			{-0.5, -0.5, -0.5, -0.375, 0.125, -0.375},
+			{0.375, -0.5, -0.5, 0.5, 0.125, -0.375},
 		}
 	},
 	selection_box = {
 		type = "fixed",
 		fixed = {
-			{-0.5, 0, -0.5, 0.5, 0.125, 0.5}, -- NodeBox10
-			{0.375, -0.5, 0.375, 0.5, 0.125, 0.5}, -- NodeBox11
-			{-0.5, -0.5, 0.375, -0.375, 0.125, 0.5}, -- NodeBox12
-			{-0.5, -0.5, -0.5, -0.375, 0.125, -0.375}, -- NodeBox13
-			{0.375, -0.5, -0.5, 0.5, 0.125, -0.375}, -- NodeBox14
+			{-0.5, 0, -0.5, 0.5, 0.125, 0.5},
+			{0.375, -0.5, 0.375, 0.5, 0.125, 0.5},
+			{-0.5, -0.5, 0.375, -0.375, 0.125, 0.5},
+			{-0.5, -0.5, -0.5, -0.375, 0.125, -0.375},
+			{0.375, -0.5, -0.5, 0.5, 0.125, -0.375},
 		}
 	}
 })
